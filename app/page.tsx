b60eac8aa89a9776f7a56b62cf8283379eba5aa7
vivek <vivekkumarsharma@mastroke.com>
2026-04-08 09:06:54 +0000

    update text

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen gap-6">
-      <h1 className="text-3xl font-bold">Hello Vivek 🚀</h1>
-
-      <p className="text-lg">My first Next.js app running on CodeSandbox</p>
-
-      <Image src="/next.svg" alt="Next.js logo" width={120} height={30} />
+      <main style={{ padding: "40px" }}>
+        <h1>Welcome to Mastroke 🚀</h1>
+        <p>This is our new Next.js website By Vivek Kumar Sharma </p>
+      </main>
     </div>
   );
 }
