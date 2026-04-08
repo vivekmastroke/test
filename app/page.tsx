d41c0a805e4fe27b4ec843d08c23c1ddbdc16ff4
vivek <vivekkumarsharma@mastroke.com>
2026-04-08 09:10:14 +0000

    change message

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center min-h-screen gap-6">
       <main style={{ padding: "40px" }}>
         <h1>Welcome to Mastroke 🚀</h1>
-        <p>This is our new Next.js website By Vivek Kumar Sharma </p>
+        <p>This is our new Next.js website By Vivek Kumar Sharma hello </p>
       </main>
     </div>
   );
